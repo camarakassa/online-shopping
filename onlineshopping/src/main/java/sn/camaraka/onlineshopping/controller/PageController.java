886ead0d80877604aeecf323d237becd70fc5033
camarakassa <camarakassa93@gmail.com>
2019-02-03 23:02:56 +0000

    Ressources Statiques Chargées avec Succès

---
+++ onlineshopping/src/main/java/sn/camaraka/onlineshopping/controller/PageController.java
@@ -0,0 +1,19 @@
+package sn.camaraka.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("accueil", "Welcome to spring  Web MVC");
+		return mv;
+	}
+
+}
